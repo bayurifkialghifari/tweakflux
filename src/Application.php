9a359cab067a73f7f5469471def204ea366fd938
bayurifkialghifari <bayurifkialgh@gmail.com>
2026-05-12 09:18:40 +0700

    add check symfony console version

--- src/Application.php
+++ src/Application.php
@@ -20,10 +20,21 @@ final class Application extends SymfonyApplication
 
         parent::__construct('TweakFlux', $version);
 
-        $this->addCommand(new ListCommand());
-        $this->addCommand(new ApplyCommand());
-        $this->addCommand(new CreateCommand());
-        $this->addCommand(new BoostCommand());
-        $this->addCommand(new UpdateCommand());
+        $symfonyConsoleVersion = InstalledVersions::getVersion('symfony/console');
+
+        // Add commands based on Symfony Console version
+        if (version_compare($symfonyConsoleVersion, '7.4.0', '>=')) {
+            $this->addCommand(new ListCommand());
+            $this->addCommand(new ApplyCommand());
+            $this->addCommand(new CreateCommand());
+            $this->addCommand(new BoostCommand());
+            $this->addCommand(new UpdateCommand());
+        } else {
+            $this->add(new ListCommand());
+            $this->add(new ApplyCommand());
+            $this->add(new CreateCommand());
+            $this->add(new BoostCommand());
+            $this->add(new UpdateCommand());
+        }
     }
 }
